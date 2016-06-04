1d623a0ca13b963f03b5b6927c6646c68f125196
erinpettigrew <erin.pettigrew@gmail.com>
2016-06-04 16:37:36 -0400

    Refactor image and video conditionals from view into helper methods

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -1,11 +1,21 @@
 module PostHelper
-  def video_embed_code(post)
-    unless post.video_url.empty?
-      "<iframe width='560' height='315' src='https://www.youtube.com/embed/" +
-       self.video_id(post) + "?autoplay=1' frameborder='0' allowfullscreen></iframe>"
+  def render_image(post)
+    if post.image_url.present?
+      image_tag(post.image_url)
     end
   end
-  
+
+  def render_video(post)
+    if post.video_url.present?
+      video_embed_code(post).html_safe
+    end
+  end
+
+  def video_embed_code(post)
+    "<iframe width='560' height='315' src='https://www.youtube.com/embed/" +
+     self.video_id(post) + "?autoplay=1&enablejsapi=1' frameborder='0' allowfullscreen></iframe>"
+  end
+
   def video_id(post)
     post.video_url.gsub("https://www.youtube.com/watch?v=", "")
   end
