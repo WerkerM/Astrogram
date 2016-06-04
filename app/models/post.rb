195edb477283b1cabc1a286b3766b25c8746421d
erinpettigrew <erin.pettigrew@gmail.com>
2016-06-04 13:33:56 -0400

    Create post forms and views; embed post form on homepage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ApplicationRecord
-  belongs_to :astronaut, class_name: "User"
+  # belongs_to :astronaut, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: "astronaut_id"
+  # has_many :posts, foreign_key: "astronaut_id"
   has_many :follows, foreign_key: "user_id"
   has_many :follows, foreign_key: "astronaut_id"
   has_many :comments
