40522f00fd26bd7619052881d48b93d5dbba6dec
Michael Appell <mikeappell@gmail.com>
2016-06-05 16:33:18 -0400

    Sessions functioning, login/logout. Navbar. Error partial.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :current_user
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    if logged_in?
+      @current_user ||= User.find(session[:user_id])
+    else
+      false
+    end
+  end
+
+  def logged_in?
+    session[:user_id] ? true : false
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def navbar_logged_in
+    if logged_in?
+      "<span class='pull-right'>#{link_to(@current_user.name, @current_user)} | #{link_to('Log Out', '/logout', method: 'delete')}</span>".html_safe
+    else
+      "<span class='pull-right'>#{link_to('Sign In', '/login')}</span>".html_safe
+    end
+  end
 end
