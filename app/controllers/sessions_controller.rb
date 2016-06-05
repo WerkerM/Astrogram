3b17c7c9b32b3896009ae2da33dfaabbcc910d96
Michael Appell <mikeappell@gmail.com>
2016-06-05 17:30:00 -0400

    Fixed navbar css formatting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: login_params[:username])
     if @user && @user.authenticate(login_params[:password])
       session[:user_id] = @user.id
-      redirect_to posts_path, notice: "You have successfully logged in! Shine on, you crazy diamond."
+      redirect_to root_path, notice: "You have successfully logged in! Shine on, you crazy diamond."
     else
       @user = User.new
       flash.now[:alert] = "There was a problem with your login; please try again."
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def navbar_logged_in
     if logged_in?
-      "<span class='pull-right'>#{link_to(@current_user.name, @current_user)} | #{link_to('Log Out', '/logout', method: 'delete')}</span>".html_safe
+      "<div>#{link_to(@current_user.name, @current_user)} | #{link_to('Log Out', '/logout', method: 'delete')}</div>".html_safe
     else
-      "<span class='pull-right'>#{link_to('Sign In', '/login')}</span>".html_safe
+      "<div>#{link_to('Sign In', '/login')}</div>".html_safe
     end
   end
 end
