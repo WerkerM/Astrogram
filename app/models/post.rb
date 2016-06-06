416c652505a5d000b174d572c02d5171d3b705de
Michael Appell <mikeappell@gmail.com>
2016-06-06 11:45:51 -0400

    Added model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,8 @@
 class Post < ApplicationRecord
+  include ActiveModel::Validations
+
   belongs_to :astronaut, class_name: "User"
   has_many :comments
+  validates_with PosterIsAstronaut
 
-  validate :poster_is_astronaut
-
-  def poster_is_astronaut
-    unless astronaut.astronaut?
-      errors.add(:astronaut, "Poster needs to be an astronaut")
-    end
-  end
 end
