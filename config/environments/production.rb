7248a1f32618c77c8d1c0eb6163e6cc3ab85bc05
Michael Handwerker <michael.handwerker@gmail.com>
2016-06-06 14:27:27 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
     port:                 587,
     domain:               'gmail.com',
     user_name:            'astrogram.mailer.gmail.com',
-    password:             'upupandaway!',
+    password:             ENV,
     authentication:       'plain',
     openssl_verify_mode:  'none',
     enable_starttls_auto: true  }
