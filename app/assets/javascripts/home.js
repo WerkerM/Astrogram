6c3bb7865db9f5955bd2f22c570ef48fa8aef13b
erinpettigrew <erin.pettigrew@gmail.com>
2016-06-05 22:03:33 -0400

    Add event listener for cleared media handler field and hide fields that aren't needed

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -8,6 +8,13 @@ function inputHandler() {
       var imageUrl = $('#post_image_url').val()
       $('media-preview').html('')
       $('.media-preview').html('<img src=' + imageUrl + '>')
+      $('#post_video_url').hide()
+    }
+    if (event.keyCode == 8) {
+      if ($('#post_image_url').val() == "") {
+        $('.media-preview').html('')
+        $('#post_video_url').show()
+      }
     }
   })
 
@@ -17,6 +24,13 @@ function inputHandler() {
       var videoId = parseVideoUrl(videoUrl)
       $('media-preview').html('')
       $('.media-preview').html("<iframe width='560' height='315' src='https://www.youtube.com/embed/" + videoId + "?autoplay=1&enablejsapi=1' frameborder='0' allowfullscreen></iframe>")
+      $('#post_image_url').hide()
+    }
+    if (event.keyCode == 8) {
+      if ($('#post_video_url').val() == "") {
+        $('.media-preview').html('')
+        $('#post_image_url').show()
+      }
     }
   })
 }
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.create(comment_params)
-    redirect_to :home
+    redirect_to root_path
   end
 
   private
