5775549e82db46242fbef70a43fab59e0d58ab38
erinpettigrew <erin.pettigrew@gmail.com>
2016-06-07 11:25:46 -0400

    Refactor feed preparation and filtering into service object FilterFeed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,8 @@ class HomeController < ApplicationController
 
   def index
     if current_user
+      @posts = PersonalizeFeed.create(current_user)
       @post = current_user.posts.build
-      followed_astronaut_ids = current_user.following.pluck(:astronaut_id)
-      @posts = Post.where(astronaut_id: followed_astronaut_ids)
     else
       @posts = Post.last(5)
     end
--- app/services/personalize_feed.rb
+++ app/services/personalize_feed.rb
@@ -1,6 +1,6 @@
 class PersonalizeFeed
-
-  def self.create
+  def self.create(user)
+    followed_ids = user.following.pluck(:astronaut_id)
+    @posts = Post.where(astronaut_id: followed_ids)
   end
-
 end
