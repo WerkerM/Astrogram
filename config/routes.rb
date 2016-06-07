6a6f2cb79840602ee83f83734a4b09695106fbc5
Michael Handwerker <michael.handwerker@gmail.com>
2016-06-07 11:22:35 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  get '*path' => redirect('/'), alert: "Page does not exist"
+  get '*path' => redirect('/')
 end
