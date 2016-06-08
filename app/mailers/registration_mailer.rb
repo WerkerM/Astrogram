180c53113e56024aa75a9508e5e314ef48c8c5a2
WerkerM <michael.handwerker@gmail.com>
2016-06-08 15:01:35 -0400

    refactored mailer controller

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -2,15 +2,8 @@ class RegistrationMailer < ApplicationMailer
   default from: 'astrogram.mailer@gmail.com'
 
   def confirmation_email(user)
-    token = user.gen_email_token
-    user.email_token = token
-    user.save!
-    @user = user
-    if Rails.env.production?
-      @url  = "http://astrogram.herokuapp.com/confirm_email/#{token}"
-    else
-      @url  = "http://localhost:3000/confirm_email/#{token}"
-    end
+    token, @user = EmailTokenGenerator.generate(user)
+    @url = EnvironmentSelector.host_url(token)
     mail(from: 'astrogram.mailer@gmail.com', to: @user.email, subject: 'Verify Your Astrogram Account!')
   end
 
