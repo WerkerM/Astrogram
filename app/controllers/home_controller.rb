d10734d30b066f5948b94d697b4e04c39f29cc4c
Michael Appell <mikeappell@gmail.com>
2016-06-08 08:03:50 -0400

    Made post index pages show newest content first

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   skip_before_action :redirect_unauthenticated_user
 
   def index
-    @posts = FeedPersonalizer.create(current_user)
+    @posts = FeedPersonalizer.create(current_user).order(created_at: :DESC)
     if current_user
       @post = current_user.posts.build
       @post.space_tag = SpaceTag.new
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :DESC)
   end
 
   def show
