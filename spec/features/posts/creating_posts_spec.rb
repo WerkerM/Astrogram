d6c77599e28935b622fa8c25c645a514db516330
Michael Appell <mikeappell@gmail.com>
2016-06-08 13:02:06 -0400

    One more test, for old time's sake

--- spec/features/posts/creating_posts_spec.rb
+++ spec/features/posts/creating_posts_spec.rb
@@ -3,17 +3,17 @@ describe "creating posts", js: true do
 
   context "when the user is an astronaut" do
 
-    it "shows a form for a new post" do
-      visit user_path
-      #check for the form
-    end
-  end
+  #   it "shows a form for a new post" do
+  #     visit user_path
+  #     #check for the form
+  #   end
+  # end
 
-  context "when the user is an earthling" do
+  # context "when the user is an earthling" do
 
-    it "does not show a form for a new post" do
-      visit user_path
-      #check for the form
-    end
+  #   it "does not show a form for a new post" do
+  #     visit user_path
+  #     #check for the form
+  #   end
   end
 end
