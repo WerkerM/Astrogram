0b7bf83bad998ce2f4bf49985f4c0158b6cbc285
Michael Alpert-Appell <mikeappell@gmail.com>
2016-06-11 12:30:21 -0400

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,4 @@
- module ApplicationHelper
+module ApplicationHelper
   def navbar_logged_in
     if logged_in?
       "<div>#{link_to(@current_user.name, @current_user)} | #{link_to('Log Out', '/logout', method: 'delete')}</div>".html_safe
